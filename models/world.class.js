2c46bb8e6fa3bf978c8e0eba4df8b5e98efcf94e
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-03-18 10:11:14 +0100

    initial commit
    - insert progamming structure from DA
    - insert imgs from DA
    - insert audio from DA
    - include modals for object orientation
    - connect the modals together
    - insert status bars

---
+++ models/world.class.js
@@ -0,0 +1,98 @@
+class World{
+    character = new Character();
+    level = level1;
+    canvas;
+    ctx;
+    keyboard;
+    camera_x = 0;
+    statusBar = new StatusBar();
+    statusBarCoins = new StatusBarCoins();
+    statusBarBottles = new StatusBarBottles();
+    throwableObjects = [new ThrowableObject()];
+    
+
+    constructor(canvas, keyboard){
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.keyboard = keyboard;
+        this.draw();
+        this.setWorld();
+        this.run();
+    }
+
+    setWorld(){
+        this.character.world = this;
+    }
+
+    run(){
+        setInterval(() => {
+            this.checkCollisions();
+            this.checkThrowObjects();
+        }, 200);
+    }
+
+    checkCollisions(){
+        this.level.enemies.forEach((enemy) =>{
+            if(this.character.isColliding(enemy)){
+                this.character.hit();
+                this.statusBar.setPercentage(this.character.energy);
+            }
+        })
+    }
+
+    checkThrowObjects(){
+        if(this.keyboard.D){
+            let bottle = new ThrowableObject(this.character.x, this.character.y)
+            this.throwableObjects.push(bottle);
+        }
+    }
+
+
+
+    draw(){
+ 
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.translate(this.camera_x, 0);
+        this.addObjectsToMap(this.level.backgroundObjects);
+
+        this.ctx.translate(-this.camera_x, 0);
+        this.addToMap(this.statusBar);
+        this.addToMap(this.statusBarCoins);
+        this.addToMap(this.statusBarBottles);
+
+        this.ctx.translate(this.camera_x, 0);
+
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.throwableObjects);
+        this.addObjectsToMap(this.level.enemies);
+        this.addObjectsToMap(this.level.clouds);
+        this.ctx.translate(-this.camera_x, 0);
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
+    }
+
+    addObjectsToMap(objects){
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.save();
+        if (mo.otherDirection) {
+            this.flipImage(mo);
+        }
+        mo.draw(this.ctx);
+        mo.drawFrame(this.ctx);
+        this.ctx.restore();
+    }
+    
+    flipImage(mo) {
+        this.ctx.translate(mo.x + mo.width / 2, mo.y);
+        this.ctx.scale(-1, 1);
+        this.ctx.translate(-mo.x - mo.width / 2, -mo.y);
+    }
+
+}
