082e54d80e6805b8f0ab0d0e27f27eb64adf500a
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-03-20 16:34:00 +0100

    implement the bottles in the game in the JS-file drawable-object.class.js / optimate the code in this file for better a reading
    - insert the botle in the game in the function drawFrame(ctx)
    - optimate the function and constructor for a better reading:
    - constructor (create an object with all the parameters)
    - loadImage (path) (create an arrow function)
    - loadImages (arr) (create an arrow function)
    - draw(ctx) (delete the if-function)
    - drawFrame(ctx) (implement the const into the if-function)

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -1,42 +1,21 @@
 class DrawableObject {
     constructor() {
-        this.x = 120;
-        this.y = 210;
-        this.height = 90;
-        this.width = 175;
-        this.img = null;
-        this.imageCache = {};
-        this.currentImage = 0;
+        Object.assign(this, { x: 120, y: 210, height: 90, width: 175, img: null, imageCache: {}, currentImage: 0 });
     }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
+    loadImage = (path) => (this.img = new Image(), this.img.src = path);
 
-    loadImages(arr) {
-        arr.forEach(path => {
-            const img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+    loadImages = (arr) => arr.forEach(path => this.imageCache[path] = Object.assign(new Image(), { src: path }));
 
     draw(ctx) {
-        if (this.img) {
-            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-        }
+        this.img && ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
     drawFrame(ctx) {
-        const frameTypes = [Character, Chicken, Endboss, CollectableCoins];
-        
-        if (frameTypes.some(type => this instanceof type)) {
-            ctx.beginPath();
-            ctx.lineWidth = 2;
+        if ([Character, Chicken, Endboss, CollectableCoins, CollectableBottle].some(cls => this instanceof cls)) {
             ctx.strokeStyle = 'green';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
+            ctx.lineWidth = 2;
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
         }
     }
 }
