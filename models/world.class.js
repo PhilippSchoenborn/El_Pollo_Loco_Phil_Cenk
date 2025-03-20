c9b177ffd34e40d940b325411ed649a6d476e7f4
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-03-20 16:48:03 +0100

    Update world.class.js
    - insert the class collectableBottles
    - insert in the constructor the spawn of the bottles and optimate the constructor for a better reading
    - insert in the function checkCollisions the option to collect the bottles in the game
    - corrected the function checkThrowObject. you can only throw bottles if you collect them. you can't throw bottles if the status bar is empty
    - update function draw() -> insert the fact that the bottles are insert in the game
    - optemate the functions addObjects(objectGroups), drawObject(mo) and spawnCoins for a better reading
    - implement a new funtion (spawnBottles()) to insert the bottles in the game

--- models/world.class.js
+++ models/world.class.js
@@ -10,6 +10,7 @@ class World {
     statusBarBottles = new StatusBarBottles();
     throwableObjects = [];
     collectableCoins = [];
+    collectableBottles = [];
 
     COIN_Y = 350;
     COIN_MIN_X = 350;
@@ -21,13 +22,12 @@ class World {
     COLLISION_CHECK_INTERVAL = 200;
 
     constructor(canvas, keyboard) {
-        this.ctx = canvas.getContext('2d');
-        this.canvas = canvas;
-        this.keyboard = keyboard;
+        Object.assign(this, { ctx: canvas.getContext('2d'), canvas, keyboard });
         this.character.world = this;
         this.run();
         this.spawnCoins();
         this.draw();
+        this.spawnBottles();
     }
 
     run() {
@@ -38,19 +38,35 @@ class World {
     }
 
     checkCollisions() {
-        this.level.enemies.forEach(e => { if (this.character.isColliding(e)) { this.character.hit(); this.statusBar.setPercentage(this.character.energy); } });
-        this.collectableCoins.forEach((c, i) => { if (this.character.isColliding(c)) { this.collectableCoins.splice(i, 1); this.statusBarCoins.increaseCoins(); } });
+        this.level.enemies.forEach(e => this.character.isColliding(e) && (this.character.hit(), this.statusBar.setPercentage(this.character.energy)));
+        this.collectableCoins = this.collectableCoins.filter(c => {
+            if (this.character.isColliding(c)) {
+                this.statusBarCoins.increaseCoins();
+                return false;
+            }
+            return true;
+        });
+        this.collectableBottles = this.collectableBottles.filter(bottle => {
+            if (this.character.isColliding(bottle)) {
+                this.statusBarBottles.increaseBottles();
+                return false;
+            }
+            return true;
+        });
     }
 
     checkThrowObjects() {
-        if (this.keyboard.D) this.throwableObjects.push(new ThrowableObject(this.character.x + this.THROW_OFFSET_X, this.character.y + this.THROW_OFFSET_Y));
+        if (this.keyboard.D && this.statusBarBottles.percentage > 0) {
+            this.throwableObjects.push(new ThrowableObject(this.character.x + this.THROW_OFFSET_X, this.character.y + this.THROW_OFFSET_Y));
+            this.statusBarBottles.decreaseBottles();
+        }
     }
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.applyCamera(() => this.addObjects([this.level.backgroundObjects]));
         this.drawStatus();
-        this.applyCamera(() => this.addObjects([this.collectableCoins, [this.character], this.throwableObjects, this.level.enemies, this.level.clouds]));
+        this.applyCamera(() => this.addObjects([this.collectableCoins, this.collectableBottles, [this.character], this.throwableObjects, this.level.enemies, this.level.clouds]));
         requestAnimationFrame(() => this.draw());
     }
 
@@ -66,12 +82,12 @@ class World {
     }
 
     addObjects(objectGroups) {
-        objectGroups.forEach(group => group.forEach(obj => this.drawObject(obj)));
+        objectGroups.flat().forEach(obj => this.drawObject(obj));
     }
 
     drawObject(mo) {
         this.ctx.save();
-        if (mo.otherDirection) this.flipImage(mo);
+        mo.otherDirection && this.flipImage(mo);
         mo.draw(this.ctx);
         mo.drawFrame(this.ctx);
         this.ctx.restore();
@@ -84,14 +100,23 @@ class World {
     }
 
     spawnCoins() {
-        for (let i = 0; i < this.COIN_COUNT; i++) {
+        this.collectableCoins = Array.from({ length: this.COIN_COUNT }, () => {
             let x;
-            do { x = this.COIN_MIN_X + Math.random() * (this.COIN_MAX_X - this.COIN_MIN_X); } while (this.checkOverlap(x));
-            this.collectableCoins.push(new CollectableCoins(x, this.COIN_Y));
-        }
+            while (this.checkOverlap(x = this.COIN_MIN_X + Math.random() * (this.COIN_MAX_X - this.COIN_MIN_X)));
+            return new CollectableCoins(x, this.COIN_Y);
+        });
     }
 
-    checkOverlap(x) {
-        return this.collectableCoins.some(c => Math.abs(x - c.x) < this.COIN_SPACING);
+    spawnBottles() {
+        const fixedY = 350;
+        this.collectableBottles = Array.from({ length: 15 }, () => {
+            let randomX;
+            while (this.checkBottleOverlap(randomX = 250 + Math.random() * 2200));
+            return new CollectableBottle(randomX, fixedY);
+        });
     }
+
+    checkOverlap = (x) => this.collectableCoins.some(c => Math.abs(x - c.x) < this.COIN_SPACING);
+
+    checkBottleOverlap = (newX) => this.collectableBottles.some(bottle => Math.abs(newX - bottle.x) < 120);
 }
