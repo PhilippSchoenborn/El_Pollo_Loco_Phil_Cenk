06a9344f1d2d65b4e44f235817fd40b36d4fa5be
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-04-05 12:42:33 +0200

    add clean code and JS docs

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,15 +1,29 @@
+/**
+ * Represents a normal enemy chicken in the game.
+ * The chicken moves left continuously and can be killed by the player.
+ */
 class Chicken extends MovableObject {
     height = 70;
     width = 65;
     y = 360;
     death_sound = new Audio('/audio/chicken_dead.mp3');
 
+    /**
+     * Array of image paths for the walking animation.
+     * @type {string[]}
+     */
     IMAGES_WALKING = [
         './img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         './img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         './img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
     ];
 
+    /**
+     * Creates a new Chicken instance at a random horizontal position.
+     * The chicken will start walking and play walking animation by default.
+     * 
+     * @param {number} [x=200 + Math.random() * 500] - The starting x position of the chicken.
+     */
     constructor(x = 200 + Math.random() * 500) {
         super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
@@ -22,16 +36,24 @@ class Chicken extends MovableObject {
         this.animate();
     }
 
+    /**
+     * Starts the chicken's movement and walking animation.
+     * The chicken will move left and play the walking animation in a loop.
+     */
     animate() {
         this.moveInterval = setInterval(() => {
             this.moveLeft();
         }, 1000 / 60);
-
         this.animationInterval = setInterval(() => {
             this.playAnimation(this.IMAGES_WALKING);
         }, 125);
     }
 
+    /**
+     * Handles the chicken's death.
+     * Stops the chicken's movement and animation, changes the image to the dead state,
+     * and plays the death sound effect.
+     */
     die() {
         this.loadImage('./img/3_enemies_chicken/chicken_normal/2_dead/dead.png');
         clearInterval(this.moveInterval);
