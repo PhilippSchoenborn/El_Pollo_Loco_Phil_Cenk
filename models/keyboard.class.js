515e76ad19b57312d802359e5e202a78d43a43ee
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-04-05 12:42:49 +0200

    add clean code and JS docs

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,10 +1,41 @@
+/**
+ * Represents the state of the keyboard, tracking whether specific keys are pressed or not.
+ * This class provides a way to manage the state of keys like arrow keys, spacebar, and 'D'.
+ */
 class Keyboard {
+    /**
+     * Indicates whether the LEFT arrow key is pressed.
+     * @type {boolean}
+     */
     LEFT = false;
+
+    /**
+     * Indicates whether the RIGHT arrow key is pressed.
+     * @type {boolean}
+     */
     RIGHT = false;
+
+    /**
+     * Indicates whether the UP arrow key is pressed.
+     * @type {boolean}
+     */
     UP = false;
+
+    /**
+     * Indicates whether the DOWN arrow key is pressed.
+     * @type {boolean}
+     */
     DOWN = false;
+
+    /**
+     * Indicates whether the SPACE key is pressed.
+     * @type {boolean}
+     */
     SPACE = false;
-    D = false; 
 
-
+    /**
+     * Indicates whether the 'D' key is pressed.
+     * @type {boolean}
+     */
+    D = false;
 }
