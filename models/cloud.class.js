320ca237820f4ec22e80daecf6a983770d8ddfa6
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-04-05 12:42:35 +0200

    add clean code and JS docs

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,3 +1,7 @@
+/**
+ * Represents a cloud object that moves across the screen.
+ * The cloud is drawn repeatedly to create a seamless scrolling effect.
+ */
 class Cloud extends MovableObject {
     height = 320;
     width = 719;
@@ -8,6 +12,11 @@ class Cloud extends MovableObject {
     ];
     speed = 0.2;
 
+    /**
+     * Creates a new cloud object that moves horizontally across the canvas.
+     * 
+     * @param {number} canvasWidth - The width of the canvas used to calculate cloud segments.
+     */
     constructor(canvasWidth) {
         super();
         this.loadImages(this.images);
@@ -19,25 +28,31 @@ class Cloud extends MovableObject {
         this.animate();
     }
 
+    /**
+     * Starts the animation loop for the cloud's movement and fading effect.
+     * 
+     * @private
+     */
     animate() {
         let lastTimestamp = 0;
         const animateFrame = (timestamp) => {
             if (!lastTimestamp) lastTimestamp = timestamp;
             const deltaTime = timestamp - lastTimestamp;
             this.x -= this.speed * (deltaTime / (1000 / 60));
-            this.fadeProgress += this.speed * (deltaTime / (1000 / 60));
-            if (this.fadeProgress >= this.width) {
-                this.fadeProgress = 0;
-            }
-            if (this.x <= -this.width) {
-                this.x += this.width;
-            }
+            this.fadeProgress = (this.fadeProgress + this.speed * (deltaTime / (1000 / 60))) % this.width;
+            if (this.x <= -this.width) this.x += this.width;
             lastTimestamp = timestamp;
             requestAnimationFrame(animateFrame);
         };
         requestAnimationFrame(animateFrame);
     }
 
+    /**
+     * Draws the cloud on the given canvas context with a scrolling effect.
+     * The cloud is drawn in multiple segments with a fade effect to create a continuous movement.
+     * 
+     * @param {CanvasRenderingContext2D} ctx - The canvas 2D rendering context to draw the cloud.
+     */
     draw(ctx) {
         for (let i = 0; i < this.segments; i++) {
             const currentImage = this.imageCache[this.images[i % this.images.length]];
