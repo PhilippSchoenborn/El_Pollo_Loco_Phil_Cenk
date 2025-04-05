0fdf617669a114144017809a4f41e2c133761ece
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-04-05 12:42:40 +0200

    add clean code and JS docs

--- models/collectable-coins.class.js
+++ models/collectable-coins.class.js
@@ -1,11 +1,28 @@
+/**
+ * Represents a collectible coin object that oscillates up and down 
+ * and switches images periodically to create an animated effect.
+ */
 class CollectableCoins extends MovableObject {
+    /**
+     * Creates a new collectible coin at the specified position.
+     * The coin will oscillate up and down and alternate between two images.
+     * 
+     * @param {number} x - The x-coordinate of the coin's initial position.
+     * @param {number} y - The y-coordinate of the coin's initial position.
+     */
     constructor(x, y) {
         super().loadImage('./img/8_coin/coin_1.png');
-        Object.assign(this, { x, y, width: 100, height: 100, initialY: y});
+        Object.assign(this, { x, y, width: 100, height: 100, initialY: y });
         this.animate();
         this.switchImage();
     }
 
+    /**
+     * Animates the coin's vertical oscillation. The coin moves up and down 
+     * by a small amount (10 pixels) and changes direction when reaching the limits.
+     * 
+     * @private
+     */
     animate() {
         let direction = 1;
         setInterval(() => {
@@ -14,6 +31,12 @@ class CollectableCoins extends MovableObject {
         }, 1000 / 60);
     }
 
+    /**
+     * Periodically switches between two images to create an animation effect.
+     * The coin alternates between two images every 700ms.
+     * 
+     * @private
+     */
     switchImage() {
         let currentImageIndex = 0;
         const images = [
