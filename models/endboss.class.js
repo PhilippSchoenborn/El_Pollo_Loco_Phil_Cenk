7a0e7fe74700ff77eca30283bef5c3d381decc70
Cenk Korkmaz <Cenk.Korkmaz92@gmail.com>
2025-04-09 18:51:09 +0200

    added hitbox offset for endboss

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,263 +1,249 @@
 /**
- * Represents the Endboss, a powerful enemy character in the game.
- * Extends MovableObject and manages states, animations, attacks,
- * and interactions with the player.
+ * The Endboss enemy with states, animations, attacks, and a health bar.
  */
 class Endboss extends MovableObject {
-    height = 250;
-    width = 200;
-    y = 195;
-    speed = 1.5;
-
-    canTakeDamage = false;
-    hitPoints = 3;
-    currentState = 'waiting';
-
+    height = 250
+    width = 200
+    y = 195
+    speed = 1.5
+    canTakeDamage = false
+    hitPoints = 3
+    currentState = 'waiting'
     walkImages = [
-        './img/4_enemie_boss_chicken/1_walk/G1.png',
-        './img/4_enemie_boss_chicken/1_walk/G2.png',
-        './img/4_enemie_boss_chicken/1_walk/G3.png',
-        './img/4_enemie_boss_chicken/1_walk/G4.png'
-    ];
+      './img/4_enemie_boss_chicken/1_walk/G1.png',
+      './img/4_enemie_boss_chicken/1_walk/G2.png',
+      './img/4_enemie_boss_chicken/1_walk/G3.png',
+      './img/4_enemie_boss_chicken/1_walk/G4.png'
+    ]
     alertImages = [
-        './img/4_enemie_boss_chicken/2_alert/G5.png',
-        './img/4_enemie_boss_chicken/2_alert/G6.png',
-        './img/4_enemie_boss_chicken/2_alert/G7.png',
-        './img/4_enemie_boss_chicken/2_alert/G8.png',
-        './img/4_enemie_boss_chicken/2_alert/G9.png',
-        './img/4_enemie_boss_chicken/2_alert/G10.png',
-        './img/4_enemie_boss_chicken/2_alert/G11.png',
-        './img/4_enemie_boss_chicken/2_alert/G12.png'
-    ];
+      './img/4_enemie_boss_chicken/2_alert/G5.png',
+      './img/4_enemie_boss_chicken/2_alert/G6.png',
+      './img/4_enemie_boss_chicken/2_alert/G7.png',
+      './img/4_enemie_boss_chicken/2_alert/G8.png',
+      './img/4_enemie_boss_chicken/2_alert/G9.png',
+      './img/4_enemie_boss_chicken/2_alert/G10.png',
+      './img/4_enemie_boss_chicken/2_alert/G11.png',
+      './img/4_enemie_boss_chicken/2_alert/G12.png'
+    ]
     attackImages = [
-        './img/4_enemie_boss_chicken/3_attack/G13.png',
-        './img/4_enemie_boss_chicken/3_attack/G14.png',
-        './img/4_enemie_boss_chicken/3_attack/G15.png',
-        './img/4_enemie_boss_chicken/3_attack/G16.png',
-        './img/4_enemie_boss_chicken/3_attack/G17.png',
-        './img/4_enemie_boss_chicken/3_attack/G18.png',
-        './img/4_enemie_boss_chicken/3_attack/G19.png',
-        './img/4_enemie_boss_chicken/3_attack/G20.png'
-    ];
+      './img/4_enemie_boss_chicken/3_attack/G13.png',
+      './img/4_enemie_boss_chicken/3_attack/G14.png',
+      './img/4_enemie_boss_chicken/3_attack/G15.png',
+      './img/4_enemie_boss_chicken/3_attack/G16.png',
+      './img/4_enemie_boss_chicken/3_attack/G17.png',
+      './img/4_enemie_boss_chicken/3_attack/G18.png',
+      './img/4_enemie_boss_chicken/3_attack/G19.png',
+      './img/4_enemie_boss_chicken/3_attack/G20.png'
+    ]
     hurtImages = [
-        './img/4_enemie_boss_chicken/4_hurt/G21.png',
-        './img/4_enemie_boss_chicken/4_hurt/G22.png',
-        './img/4_enemie_boss_chicken/4_hurt/G23.png'
-    ];
+      './img/4_enemie_boss_chicken/4_hurt/G21.png',
+      './img/4_enemie_boss_chicken/4_hurt/G22.png',
+      './img/4_enemie_boss_chicken/4_hurt/G23.png'
+    ]
     deadImages = [
-        './img/4_enemie_boss_chicken/5_dead/G24.png',
-        './img/4_enemie_boss_chicken/5_dead/G25.png',
-        './img/4_enemie_boss_chicken/5_dead/G26.png'
-    ];
-
-    hit_sound = new Audio('audio/endboss_hit.mp3');
-    death_sound = new Audio('audio/chicken_dead.mp3');
-    roosterCry = new Audio('audio/roosterCry.mp3');
-
-    /**
-     * Each Endboss will have its own StatusBarEndboss instance,
-     * which we conditionally draw in World.drawStatus().
-     */
+      './img/4_enemie_boss_chicken/5_dead/G24.png',
+      './img/4_enemie_boss_chicken/5_dead/G25.png',
+      './img/4_enemie_boss_chicken/5_dead/G26.png'
+    ]
+    hit_sound = new Audio('audio/endboss_hit.mp3')
+    death_sound = new Audio('audio/chicken_dead.mp3')
+    roosterCry = new Audio('audio/roosterCry.mp3')
     constructor(x) {
-        super();
-        this.x = x;
-        this.loadImages(this.walkImages);
-        this.loadImages(this.alertImages);
-        this.loadImages(this.attackImages);
-        this.loadImages(this.hurtImages);
-        this.loadImages(this.deadImages);
-        this.loadImage(this.walkImages[0]);
-        this.hit_sound.volume = 0.5;
-        this.death_sound.volume = 0.5;
-        this.roosterCry.volume = 0.8;
-        this.otherDirection = false;
-        this.statusBar = new StatusBarEndboss();
-        this.statusBar.setPercentage(100);
-        this.animate();
+      super()
+      this.x = x
+      this.hitboxOffsetX = 30
+      this.hitboxOffsetY = 40
+      this.hitboxWidth = 140
+      this.hitboxHeight = 170
+      this.loadImages(this.walkImages)
+      this.loadImages(this.alertImages)
+      this.loadImages(this.attackImages)
+      this.loadImages(this.hurtImages)
+      this.loadImages(this.deadImages)
+      this.loadImage(this.walkImages[0])
+      this.hit_sound.volume = 0.5
+      this.death_sound.volume = 0.5
+      this.roosterCry.volume = 0.8
+      this.otherDirection = false
+      this.statusBar = new StatusBarEndboss()
+      this.statusBar.setPercentage(100)
+      this.animate()
     }
-
+  
     animate() {
-        let lastFrameTime = 0;
-        const frameInterval = 200;
-
-        const update = (timestamp) => {
-            if (this.currentState === 'entrance') {
-                if (timestamp - lastFrameTime >= frameInterval) {
-                    this.playAnimation(this.walkImages);
-                    lastFrameTime = timestamp;
-                }
-            } else if (this.currentState === 'alert') {
-                if (timestamp - lastFrameTime >= 150) {
-                    this.playAnimation(this.alertImages);
-                    lastFrameTime = timestamp;
-                }
-            } else if (this.currentState === 'chase') {
-                this.chasePlayer();
-                if (timestamp - lastFrameTime >= frameInterval) {
-                    this.playAnimation(this.walkImages);
-                    lastFrameTime = timestamp;
-                }
-            } else if (this.currentState === 'attack') {
-            }
-
-            if (this.currentState !== 'dead') {
-                requestAnimationFrame(update);
-            }
-        };
-        requestAnimationFrame(update);
+      let lastFrameTime = 0
+      const frameInterval = 200
+      const update = (timestamp) => {
+        if (this.currentState === 'entrance') {
+          if (timestamp - lastFrameTime >= frameInterval) {
+            this.playAnimation(this.walkImages)
+            lastFrameTime = timestamp
+          }
+        } else if (this.currentState === 'alert') {
+          if (timestamp - lastFrameTime >= 150) {
+            this.playAnimation(this.alertImages)
+            lastFrameTime = timestamp
+          }
+        } else if (this.currentState === 'chase') {
+          this.chasePlayer()
+          if (timestamp - lastFrameTime >= frameInterval) {
+            this.playAnimation(this.walkImages)
+            lastFrameTime = timestamp
+          }
+        } else if (this.currentState === 'attack') {}
+        if (this.currentState !== 'dead') requestAnimationFrame(update)
+      }
+      requestAnimationFrame(update)
     }
-
+  
     chasePlayer() {
-        if (!world || !world.character) return;
-        const playerX = world.character.x;
-        if (Math.abs(this.x - playerX) > 10) {
-            if (this.x < playerX) {
-                this.x += this.speed;
-                this.otherDirection = true;
-            } else {
-                this.x -= this.speed;
-                this.otherDirection = false;
-            }
-        }
-    }
-
-    doEntrance() {
-        this.currentState = 'entrance';
-        const targetX = 3300;
-        const entranceInterval = setInterval(() => {
-            if (this.x > targetX) {
-                this.x -= this.speed;
-            } else {
-                clearInterval(entranceInterval);
-                this.doAlertPhase();
-            }
-        }, 1000 / 60);
-    }
-
-    doAlertPhase() {
-        this.currentState = 'alert';
-        this.roosterCry.currentTime = 0;
-        this.roosterCry.play();
-
-        setTimeout(() => {
-            this.roosterCry.currentTime = 0;
-            this.roosterCry.play();
-            setTimeout(() => {
-                this.currentState = 'chase';
-                this.canTakeDamage = true;
-                this.statusBar.setPercentage(100);
-                world.startBossMusic();
-                world.unfreezePlayer();
-            }, 800);
-        }, 2000);
-    }
-
-    doAttack() {
-        if (this.isInInvalidState()) return;
-        this.startAttack();
-    }
-
-    isInInvalidState() {
-        return ['attack', 'dead', 'hurt'].includes(this.currentState);
-    }
-
-    startAttack() {
-        this.currentState = 'attack';
-        let frameIndex = 0;
-        const frameDelay = 150;
-
-        const attackAnim = setInterval(() => {
-            this.updateAttackFrame(frameIndex);
-            frameIndex++;
-            if (frameIndex >= this.attackImages.length) {
-                this.finishAttackAnimation(attackAnim);
-            }
-        }, frameDelay);
-    }
-
-    updateAttackFrame(frameIndex) {
-        this.img = this.imageCache[this.attackImages[frameIndex]];
-    }
-
-    finishAttackAnimation(attackAnim) {
-        clearInterval(attackAnim);
-        if (this.currentState !== 'dead') {
-            this.currentState = 'chase';
-        }
-    }
-
-    hit() {
-        if (!this.canTakeDamage || this.currentState === 'dead') return;
-        this.hitPoints--;
-        this.hit_sound.play();
-        const pct = (this.hitPoints / 3) * 100;
-        this.statusBar.setPercentage(pct);
-        if (this.hitPoints <= 0) {
-            this.die();
+      if (!world || !world.character) return
+      const playerX = world.character.x
+      if (Math.abs(this.x - playerX) > 10) {
+        if (this.x < playerX) {
+          this.x += this.speed
+          this.otherDirection = true
         } else {
-            this.currentState = 'hurt';
-            this.playHurtAnimation(() => {
-                this.currentState = 'chase';
-            });
+          this.x -= this.speed
+          this.otherDirection = false
         }
+      }
     }
-
-    playHurtAnimation(onComplete) {
-        let frameIndex = 0;
-        const frameDelay = 100;
-        const hurtAnim = setInterval(() => {
-            if (frameIndex < this.hurtImages.length) {
-                this.img = this.imageCache[this.hurtImages[frameIndex]];
-            } else {
-                clearInterval(hurtAnim);
-                onComplete();
-            }
-            frameIndex++;
-        }, frameDelay);
+  
+    doEntrance() {
+      this.currentState = 'entrance'
+      const targetX = 3300
+      const entranceInterval = setInterval(() => {
+        if (this.x > targetX) {
+          this.x -= this.speed
+        } else {
+          clearInterval(entranceInterval)
+          this.doAlertPhase()
+        }
+      }, 1000 / 60)
     }
-
-    die() {
-        this.currentState = 'dead';
-        this.speed = 0;
-        this.playDeathSound();
-        this.startDeathAnimation();
-    }
-
-    playDeathSound() {
-        this.death_sound.play();
-    }
-
-    startDeathAnimation() {
-        let frameIndex = 0;
-        const frameDelay = 350;
-        const deathAnim = setInterval(() => {
-            this.updateDeathFrame(frameIndex);
-            frameIndex++;
-            if (frameIndex >= this.deadImages.length) {
-                this.finishDeathAnimation(deathAnim);
-            }
-        }, frameDelay);
-    }
-
-    updateDeathFrame(frameIndex) {
-        this.img = this.imageCache[this.deadImages[frameIndex]];
-    }
-
-    finishDeathAnimation(deathAnim) {
-        clearInterval(deathAnim);
+  
+    doAlertPhase() {
+      this.currentState = 'alert'
+      this.roosterCry.currentTime = 0
+      this.roosterCry.play()
+      setTimeout(() => {
+        this.roosterCry.currentTime = 0
+        this.roosterCry.play()
         setTimeout(() => {
-            this.removeFromGame();
-            win();
-        }, 500);
+          this.currentState = 'chase'
+          this.canTakeDamage = true
+          this.statusBar.setPercentage(100)
+          world.startBossMusic()
+          world.unfreezePlayer()
+        }, 800)
+      }, 2000)
     }
-
+  
+    doAttack() {
+      if (this.isInInvalidState()) return
+      this.startAttack()
+    }
+  
+    isInInvalidState() {
+      return ['attack','dead','hurt'].includes(this.currentState)
+    }
+  
+    startAttack() {
+      this.currentState = 'attack'
+      let frameIndex = 0
+      const frameDelay = 150
+      const attackAnim = setInterval(() => {
+        this.updateAttackFrame(frameIndex)
+        frameIndex++
+        if (frameIndex >= this.attackImages.length) {
+          this.finishAttackAnimation(attackAnim)
+        }
+      }, frameDelay)
+    }
+  
+    updateAttackFrame(frameIndex) {
+      this.img = this.imageCache[this.attackImages[frameIndex]]
+    }
+  
+    finishAttackAnimation(attackAnim) {
+      clearInterval(attackAnim)
+      if (this.currentState !== 'dead') this.currentState = 'chase'
+    }
+  
+    hit() {
+      if (!this.canTakeDamage || this.currentState === 'dead') return
+      this.hitPoints--
+      this.hit_sound.play()
+      const pct = (this.hitPoints / 3) * 100
+      this.statusBar.setPercentage(pct)
+      if (this.hitPoints <= 0) {
+        this.die()
+      } else {
+        this.currentState = 'hurt'
+        this.playHurtAnimation(() => {
+          this.currentState = 'chase'
+        })
+      }
+    }
+  
+    playHurtAnimation(onComplete) {
+      let frameIndex = 0
+      const frameDelay = 100
+      const hurtAnim = setInterval(() => {
+        if (frameIndex < this.hurtImages.length) {
+          this.img = this.imageCache[this.hurtImages[frameIndex]]
+        } else {
+          clearInterval(hurtAnim)
+          onComplete()
+        }
+        frameIndex++
+      }, frameDelay)
+    }
+  
+    die() {
+      this.currentState = 'dead'
+      this.speed = 0
+      this.playDeathSound()
+      this.startDeathAnimation()
+    }
+  
+    playDeathSound() {
+      this.death_sound.play()
+    }
+  
+    startDeathAnimation() {
+      let frameIndex = 0
+      const frameDelay = 350
+      const deathAnim = setInterval(() => {
+        this.updateDeathFrame(frameIndex)
+        frameIndex++
+        if (frameIndex >= this.deadImages.length) {
+          this.finishDeathAnimation(deathAnim)
+        }
+      }, frameDelay)
+    }
+  
+    updateDeathFrame(frameIndex) {
+      this.img = this.imageCache[this.deadImages[frameIndex]]
+    }
+  
+    finishDeathAnimation(deathAnim) {
+      clearInterval(deathAnim)
+      setTimeout(() => {
+        this.removeFromGame()
+        win()
+      }, 500)
+    }
+  
     removeFromGame() {
-        world.level.enemies = world.level.enemies.filter(e => e !== this);
+      world.level.enemies = world.level.enemies.filter(e => e !== this)
     }
-
+  
     setMute(muted) {
-        this.hit_sound.muted = muted;
-        this.death_sound.muted = muted;
-        this.roosterCry.muted = muted;
+      this.hit_sound.muted = muted
+      this.death_sound.muted = muted
+      this.roosterCry.muted = muted
     }
-}
+  }
+  
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,201 +1,133 @@
 /**
- * Represents a movable object that can interact with the game world.
- * This class extends `DrawableObject` and adds movement, collision detection, gravity, and health management features.
+ * A movable object that extends DrawableObject and provides movement, collision, gravity, etc.
  */
 class MovableObject extends DrawableObject {
-    /**
-     * The horizontal speed of the object (default value is 0.15).
-     * @type {number}
-     */
-    speed = 0.15;
+    speed = 0.15
+    otherDirection = false
+    speedY = 0
+    energy = 100
+    lastHit = 0
+    gravityInterval = null
 
     /**
-     * A flag indicating whether the object is moving in the opposite direction (default is false).
-     * @type {boolean}
-     */
-    otherDirection = false;
-
-    /**
-     * The vertical speed of the object, which is affected by gravity (default is 0).
-     * @type {number}
-     */
-    speedY = 0;
-
-    /**
-     * The energy of the object, used to track its health (default is 100).
-     * @type {number}
-     */
-    energy = 100;
-
-    /**
-     * The timestamp of the last hit the object took.
-     * @type {number}
-     */
-    lastHit = 0;
-
-    /**
-     * The interval ID for the gravity simulation.
-     * @type {?number}
-     */
-    gravityInterval = null;
-
-    /**
-     * Starts gravity simulation for the object with a custom acceleration and interval.
-     * The object will fall based on its `speedY` and the specified `acceleration`.
-     * @param {number} acceleration - Rate at which the object falls (default is 2.5).
-     * @param {number} intervalMs - Interval in milliseconds at which gravity is applied (default is 40ms).
+     * Starts gravity with a given acceleration and interval.
+     * @param {number} acceleration
+     * @param {number} intervalMs
      */
     applyGravityWith(acceleration = 2.5, intervalMs = 40) {
         this.gravityInterval = setInterval(() => {
             if (this.shouldFall()) {
-                this.y -= this.speedY;
-                this.speedY -= acceleration;
+                this.y -= this.speedY
+                this.speedY -= acceleration
             } else {
-                // <<< New lines below >>>
-                // Force Pepe to snap to the ground and reset vertical speed
-                this.y = 210;  // or whatever your "ground" y-level is
-                this.speedY = 0;
+                this.y = 210
+                this.speedY = 0
             }
-        }, intervalMs);
+        }, intervalMs)
     }
 
-
     /**
-     * Determines if the object should fall, either because it's above the ground or still moving downward.
-     * @returns {boolean} `true` if the object should fall, `false` otherwise.
+     * Returns true if the object should fall.
+     * @returns {boolean}
      */
     shouldFall() {
-        return this.isAboveGround() || this.speedY > 0;
+        return this.isAboveGround() || this.speedY > 0
     }
 
     /**
-     * Determines if the object is above the ground (based on the `y` coordinate).
-     * @returns {boolean} `true` if the object is above the ground, `false` otherwise.
+     * Checks if object is above ground.
+     * @returns {boolean}
      */
     isAboveGround() {
-        return this instanceof ThrowableObject || this.y < 193;
+        return this instanceof ThrowableObject || this.y < 193
     }
 
-    /**
-     * Moves the object to the right by its horizontal speed.
-     */
+    /** Moves right by this.speed. */
     moveRight() {
-        this.x += this.speed;
+        this.x += this.speed
     }
 
-    /**
-     * Moves the object to the left by its horizontal speed.
-     */
+    /** Moves left by this.speed. */
     moveLeft() {
-        this.x -= this.speed;
+        this.x -= this.speed
     }
 
-    /**
-     * Makes the object jump by setting its vertical speed (`speedY`).
-     * A positive `speedY` simulates the jump.
-     */
+    /** Sets a jump impulse. */
     jump() {
-        this.speedY = 30;
+        this.speedY = 30
     }
 
     /**
-     * Plays the next animation frame from a set of images.
-     * @param {Array<string>} images - An array of image paths to display in the animation.
+     * Plays the next frame in an animation array.
+     * @param {string[]} images
      */
     playAnimation(images) {
-        const index = this.currentImage % images.length;
-        this.img = this.imageCache[images[index]];
-        this.currentImage++;
+        const index = this.currentImage % images.length
+        this.img = this.imageCache[images[index]]
+        this.currentImage++
     }
 
     /**
-     * Checks if the current object is colliding with another movable object.
-     * @param {MovableObject} mo - The movable object to check for collision.
-     * @returns {boolean} `true` if the objects are colliding, `false` otherwise.
+     * Checks collision with another MovableObject.
+     * @param {MovableObject} mo
+     * @returns {boolean}
      */
     isColliding(mo) {
-        return (
-            this.right() > mo.left() &&
-            this.bottom() > mo.top() &&
-            this.left() < mo.right() &&
-            this.top() < mo.bottom()
-        );
+        return this.right() > mo.left() && this.bottom() > mo.top() && this.left() < mo.right() && this.top() < mo.bottom()
     }
 
-    /**
-  * Gets the left position of the object for collision detection.
-  * If a custom hitbox is defined, uses the hitbox offset.
-  * @returns {number} The left position.
-  */
+    /** @returns {number} Left boundary (with optional offsets). */
     left() {
         if (this.hitbox) {
-            return this.x + this.hitbox.offsetX;
+            return this.x + this.hitbox.offsetX
         }
-        return this.x + (this.hitboxOffsetX || 0);
+        return this.x + (this.hitboxOffsetX || 0)
     }
 
-    /**
-     * Gets the right position of the object for collision detection.
-     * If a custom hitbox is defined, adds the hitbox width.
-     * @returns {number} The right position.
-     */
+    /** @returns {number} Right boundary (with optional offsets). */
     right() {
         if (this.hitbox) {
-            return this.x + this.hitbox.offsetX + this.hitbox.width;
+            return this.x + this.hitbox.offsetX + this.hitbox.width
         }
-        return this.left() + (this.hitboxWidth || this.width);
+        return this.left() + (this.hitboxWidth || this.width)
     }
 
-    /**
-     * Gets the top position of the object for collision detection.
-     * @returns {number} The top position.
-     */
+    /** @returns {number} Top boundary (with optional offsets). */
     top() {
         if (this.hitbox) {
-            return this.y + this.hitbox.offsetY;
+            return this.y + this.hitbox.offsetY
         }
-        return this.y + (this.hitboxOffsetY || 0);
+        return this.y + (this.hitboxOffsetY || 0)
     }
 
-    /**
-     * Gets the bottom position of the object for collision detection.
-     * @returns {number} The bottom position.
-     */
+    /** @returns {number} Bottom boundary (with optional offsets). */
     bottom() {
         if (this.hitbox) {
-            return this.y + this.hitbox.offsetY + this.hitbox.height;
+            return this.y + this.hitbox.offsetY + this.hitbox.height
         }
-        return this.top() + (this.hitboxHeight || this.height);
+        return this.top() + (this.hitboxHeight || this.height)
     }
 
-
-    /**
-     * Reduces the energy of the object when it is hit.
-     * The energy cannot go below 0.
-     */
+    /** Lowers energy when hit, can’t go below 0. */
     hit() {
-        this.energy = Math.max(this.energy - 5, 0);
-        if (this.energy > 0) {
-            this.lastHit = Date.now();
-        }
+        this.energy = Math.max(this.energy - 5, 0)
+        if (this.energy > 0) this.lastHit = Date.now()
     }
 
     /**
-     * Determines whether the object is currently hurt.
-     * An object is considered hurt if it was hit within the last 0.5 seconds.
-     * @returns {boolean} `true` if the object is hurt, `false` otherwise.
+     * Object is hurt if hit within last 0.5s.
+     * @returns {boolean}
      */
     isHurt() {
-        const timePassed = (Date.now() - this.lastHit) / 1000;
-        return timePassed < 0.5;
+        const timePassed = (Date.now() - this.lastHit) / 1000
+        return timePassed < 0.5
     }
 
     /**
-     * Checks if the object is dead.
-     * The object is considered dead if its energy reaches 0.
-     * @returns {boolean} `true` if the object is dead, `false` otherwise.
+     * Object is dead if energy is 0.
+     * @returns {boolean}
      */
     isDead() {
-        return this.energy === 0;
+        return this.energy === 0
     }
 }
